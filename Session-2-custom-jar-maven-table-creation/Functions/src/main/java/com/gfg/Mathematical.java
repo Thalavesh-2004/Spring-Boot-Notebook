0559277203d74a7bcd4036963875fc13b278919e
Thalavesh-2004 <thalavesh23@gmail.com>
2026-02-26 07:34:02 +0530

    custome jar creation & table creation jdbc

---
+++ Session-2-custom-jar-maven-table-creation/Functions/src/main/java/com/gfg/Mathematical.java
@@ -0,0 +1,12 @@
+package com.gfg;
+
+public class Mathematical {
+    public static int add(int a ,int b){return a+b;}
+    public static int subtract(int a,int b){
+        return a-b;
+    }
+    public static int multi(int a,int b){
+        return a*b;
+    }
+
+}
